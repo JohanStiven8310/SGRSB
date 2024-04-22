f0651a6efb6675110ba8b06e37c56ddb1729a767
JohanStiven8310 <jhanstivn93@gmail.com>
2024-04-22 15:57:05 -0500

    Modelo "reserva" pertenece a un cliente

--- app/Models/Reserva.php
+++ app/Models/Reserva.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Reserva extends Model
 {
+    use HasFactory;
+
+    protected $fillable = ['cliente_id', 'servicio_id', 'fecha_reserva', 'hora_reserva', 'comentarios'];
+
     public function cliente()
     {
         return $this->belongsTo(Cliente::class);
