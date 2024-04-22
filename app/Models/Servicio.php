af9075120fd8832da61a48a3db40c572708f7367
JohanStiven8310 <jhanstivn93@gmail.com>
2024-04-22 15:56:04 -0500

    Modelo "servicio" Multiples reservas

--- app/Models/Servicio.php
+++ app/Models/Servicio.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Servicio extends Model
 {
+    use HasFactory;
+
+    protected $fillable = ['nombre', 'descripcion', 'duracion', 'precio'];
+
     public function reservas()
     {
         return $this->hasMany(Reserva::class);
