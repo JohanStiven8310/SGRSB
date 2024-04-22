3e48f5c2f098bbabbfa75cd3ce92c4c27ffaa472
JohanStiven8310 <jhanstivn93@gmail.com>
2024-04-22 15:59:43 -0500

    Controlador "reserva"

--- app/Http/Controllers/ReservaController.php
+++ app/Http/Controllers/ReservaController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reserva;
 use Illuminate\Http\Request;
 
-class ReservaController extends Controller
+class ReservasController extends Controller
 {
-    //
+    public function index()
+    {
+        $reservas = Reserva::all();
+        return view('reservas.index', compact('reservas'));
+    }
+
+    // Implementa las funciones restantes para las operaciones CRUD según sea necesario
 }
