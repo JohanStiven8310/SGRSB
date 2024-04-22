264e3a13bacbc0cff2957998efb7faecc7271dda
JohanStiven8310 <jhanstivn93@gmail.com>
2024-04-22 15:54:39 -0500

    Modelo "cliente" multiples reservas

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Cliente extends Model
 {
+    use HasFactory;
+
+    protected $fillable = ['nombre', 'apellido', 'telefono', 'email'];
+
     public function reservas()
     {
         return $this->hasMany(Reserva::class);
