28cd9990faa4d057e1ae2d151579ee6645bb5394
JohanStiven8310 <jhanstivn93@gmail.com>
2024-04-24 11:56:52 -0500

    View "Create.blade" Servicios

--- resources/views/servicios/create.blade.php
+++ resources/views/servicios/create.blade.php
@@ -1,3 +1,19 @@
-<div>
-    <!-- The best way to take care of the future is to take care of the present moment. - Thich Nhat Hanh -->
-</div>
+<!-- resources/views/servicios/create.blade.php -->
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Crear Servicio</div>
+
+                    <div class="card-body">
+                        <!-- Agrega un formulario para crear un nuevo servicio -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
