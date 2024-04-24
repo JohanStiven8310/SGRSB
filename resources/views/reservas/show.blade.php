c8dc4372377cfc08685130bd8c29216b71664464
JeancarlosJR <jcqm757@gmail.com>
2024-04-24 12:14:12 -0500

    View "show.blade" Reservas

--- resources/views/reservas/show.blade.php
+++ resources/views/reservas/show.blade.php
@@ -1,3 +1,12 @@
-<div>
-    <!-- Act only according to that maxim whereby you can, at the same time, will that it should become a universal law. - Immanuel Kant -->
-</div>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Detalles de la Reserva</h1>
+        <p><strong>Cliente:</strong> {{ $reserva->cliente->nombre }}</p>
+        <p><strong>Servicio:</strong> {{ $reserva->servicio->nombre }}</p>
+        <p><strong>Fecha:</strong> {{ $reserva->fecha_reserva }}</p>
+        <p><strong>Hora:</strong> {{ $reserva->hora_reserva }}</p>
+        <p><strong>Comentarios:</strong> {{ $reserva->comentarios }}</p>
+    </div>
+@endsection
