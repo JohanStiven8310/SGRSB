6ba64c9b188d83f7559dbc0edb9784753f7799ce
JeancarlosJR <jcqm757@gmail.com>
2024-04-24 11:44:22 -0500

    view "edit.blade"

--- resources/views/clientes/edit.blade.php
+++ resources/views/clientes/edit.blade.php
@@ -1,3 +1,31 @@
-<div>
-    <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
-</div>
+<!-- resources/views/clientes/edit.blade.php -->
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Editar Cliente</div>
+
+                    <div class="card-body">
+                        <form action="{{ route('clientes.update', $cliente->id) }}" method="POST">
+                            @csrf
+                            @method('PUT')
+
+                            <!-- Aquí van los campos de edición del cliente -->
+                            <div class="form-group">
+                                <label for="nombre">Nombre</label>
+                                <input type="text" name="nombre" id="nombre" class="form-control" value="{{ $cliente->nombre }}">
+                            </div>
+                            <!-- Agrega más campos según los datos del cliente -->
+
+                            <button type="submit" class="btn btn-primary">Actualizar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
