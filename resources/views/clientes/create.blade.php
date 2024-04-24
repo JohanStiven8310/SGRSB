52aec415598c4150ba9347f6985c90529e93a629
JeancarlosJR <jcqm757@gmail.com>
2024-04-24 11:45:08 -0500

    view "create.blade"

--- resources/views/clientes/create.blade.php
+++ resources/views/clientes/create.blade.php
@@ -1,3 +1,30 @@
-<div>
-    <!-- If you do not have a consistent goal in life, you can not live it in a consistent way. - Marcus Aurelius -->
-</div>
+<!-- resources/views/clientes/create.blade.php -->
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Crear Cliente</div>
+
+                    <div class="card-body">
+                        <form action="{{ route('clientes.store') }}" method="POST">
+                            @csrf
+
+                            <!-- Aquí van los campos de creación del cliente -->
+                            <div class="form-group">
+                                <label for="nombre">Nombre</label>
+                                <input type="text" name="nombre" id="nombre" class="form-control">
+                            </div>
+                            <!-- Agrega más campos según los datos del cliente -->
+
+                            <button type="submit" class="btn btn-primary">Crear</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
