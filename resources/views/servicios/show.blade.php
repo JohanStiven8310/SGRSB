95e5976eb9cc68fcd7e4283e9a5eb9b7d2b5714b
JohanStiven8310 <jhanstivn93@gmail.com>
2024-04-24 11:59:14 -0500

    View "show.blade" Sevicios

--- resources/views/servicios/show.blade.php
+++ resources/views/servicios/show.blade.php
@@ -1,3 +1,21 @@
-<div>
-    <!-- Do what you can, with what you have, where you are. - Theodore Roosevelt -->
-</div>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Detalles del Servicio</div>
+
+                    <div class="card-body">
+                        <p><strong>Nombre:</strong> {{ $servicio->nombre }}</p>
+                        <p><strong>Descripción:</strong> {{ $servicio->descripcion }}</p>
+                        <p><strong>Duración:</strong> {{ $servicio->duracion }}</p>
+                        <p><strong>Precio:</strong> {{ $servicio->precio }}</p>
+                        <!-- Agrega más detalles del servicio según sea necesario -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
