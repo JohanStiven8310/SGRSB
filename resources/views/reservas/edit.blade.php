aac91a3d7bad3adafb3b73415be1c39a45352d45
JeancarlosJR <jcqm757@gmail.com>
2024-04-24 12:14:48 -0500

    View "edit.blade" Reservas

--- resources/views/reservas/edit.blade.php
+++ resources/views/reservas/edit.blade.php
@@ -1,3 +1,12 @@
-<div>
-    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
-</div>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Editar Reserva</h1>
+        <form action="{{ route('reservas.update', $reserva->id) }}" method="POST">
+            @csrf
+            @method('PUT')
+            <!-- Aquí van los campos del formulario para editar la reserva -->
+        </form>
+    </div>
+@endsection
