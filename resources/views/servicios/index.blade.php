de668db5ec41aed9ac70134475e8868931d46330
JohanStiven8310 <jhanstivn93@gmail.com>
2024-04-24 11:56:09 -0500

    View "index.blade" servicios

--- resources/views/servicios/index.blade.php
+++ resources/views/servicios/index.blade.php
@@ -1,3 +1,40 @@
-<div>
-    <!-- Do what you can, with what you have, where you are. - Theodore Roosevelt -->
-</div>
+<!-- resources/views/servicios/index.blade.php -->
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Listado de Servicios</div>
+
+                    <div class="card-body">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Nombre</th>
+                                    <th>Precio</th>
+                                    <!-- Agrega más columnas según tus necesidades -->
+                                    <th>Acciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($servicios as $servicio)
+                                <tr>
+                                    <td>{{ $servicio->nombre }}</td>
+                                    <td>{{ $servicio->precio }}</td>
+                                    <!-- Agrega más columnas según tus necesidades -->
+                                    <td>
+                                        <!-- Agrega enlaces para editar y eliminar cada servicio -->
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
