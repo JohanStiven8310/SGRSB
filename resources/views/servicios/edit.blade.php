9ceecad512b7ab648e3ba031b7e4353ac6ae4a27
JohanStiven8310 <jhanstivn93@gmail.com>
2024-04-24 11:57:22 -0500

    View "Edit.blade" Sevicios

--- resources/views/servicios/edit.blade.php
+++ resources/views/servicios/edit.blade.php
@@ -1,3 +1,19 @@
-<div>
-    <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
-</div>
+<!-- resources/views/servicios/edit.blade.php -->
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Editar Servicio</div>
+
+                    <div class="card-body">
+                        <!-- Agrega un formulario para editar un servicio existente -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
